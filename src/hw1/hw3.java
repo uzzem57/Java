daa0a3b454d3f570e9055783bc4c7ec8a139171f
Denis Dunaev <uzzem57@mail.ru>
2023-03-04 00:12:33 +0300

    add hw3.java

---
+++ src/hw1/hw3.java
@@ -0,0 +1,15 @@
+package hw1;
+import java.util.Scanner;
+
+public class hw3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое число :");
+        String a = scanner.nextLine();
+        System.out.println("Введите второе число :");
+        String b = scanner.nextLine();
+        String str = Integer.toBinaryString(Integer.parseInt(a, 2) + Integer.parseInt(b, 2));
+        System.out.println("Sum as a binary string - " + str);
+    }
+
+}
