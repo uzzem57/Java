0fd6acffbda79a5cc7d94266a83789645063ab5e
Denis Dunaev <uzzem57@mail.ru>
2023-03-03 23:52:29 +0300

    add hw2.java

---
+++ src/hw1/hw2.java
@@ -0,0 +1,20 @@
+package hw1;
+import java.util.Scanner;
+public class hw2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите числа");
+        int sum= 0;
+        int post = 0;
+        int sum_out = 0;
+        while((post = scanner.nextInt()) != 0) {
+            if (post > 0) {
+                sum = post;
+            }
+            else {
+                sum_out+=sum;
+            }
+        }
+        System.out.println(sum_out);
+        }
+}
