eb55ad4da92d00fb67125a0679828bb9bcd118a6
Denis Dunaev <uzzem57@mail.ru>
2023-03-09 01:25:51 +0300

    add hw2.java

---
+++ src/Seminar2/hw2.java
@@ -0,0 +1,29 @@
+package Seminar2;
+
+import java.util.Scanner;
+
+/**
+ * Дана последовательность из N целых чисел. Верно ли, что последовательность является возрастающей
+ */
+
+public class hw2 {
+    public static void main(String[] args) {
+        System.out.println(upper());
+    }
+    public static boolean upper() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите кол-во значений в последовательности");
+        int size = scanner.nextInt();
+        System.out.println("Введите последовательность чисел");
+        int first = scanner.nextInt();
+        for (int i = 1; i < size; i++) {
+            int next = scanner.nextInt();
+            if (next > first) {
+                first = next;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
