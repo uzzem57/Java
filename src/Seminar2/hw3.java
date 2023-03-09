50cfb1ec3448cc5af403d0828646ca256d300f05
Denis Dunaev <uzzem57@mail.ru>
2023-03-09 14:26:20 +0300

    add hw3_1.java

--- src/Seminar2/hw3.java
+++ src/Seminar2/hw3.java
@@ -8,6 +8,9 @@ import java.util.Arrays;
 public class hw3 {
     public static void main(String[] args) {
         int[] array = {1, 3, 99, 10, -4, -5, -77, 9, 8};
+        System.out.println(Arrays.toString(changeArr(array)));
+    }
+    public static int [] changeArr(int [] array){
         int sumIndex = 0;
         for (int i = 0; i < array.length; i++) {
             if (array[i] > 9 & array[i] < 100 | array[i] > -100 & array[i] < -9) {
@@ -19,6 +22,6 @@ public class hw3 {
                 array[i] = sumIndex;
             }
         }
-        System.out.println(Arrays.toString(array));
+        return array;
     }
 }
