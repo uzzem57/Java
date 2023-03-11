dc4d85a658c7d19c0c8c283e5432e2b3773c5d65
Denis Dunaev <uzzem57@mail.ru>
2023-03-11 23:31:38 +0300

    add hw4.java

--- src/Seminar3/hw4.java
+++ src/Seminar3/hw4.java
@@ -1,5 +1,9 @@
 package Seminar3;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * при входном массиве, вернуть массив массивов со всеми перестановками его элементов. (со звездочкой)
  * Пример входных данных:
@@ -9,4 +13,42 @@ package Seminar3;
  */
 
 public class hw4 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        System.out.println(combo(arr));
+    }
+
+    /**
+     * Метод реализует все возможные перестановки в массиве
+     *
+     * @param arr
+     * @return (ArrayList) myArr
+     */
+    public static ArrayList combo(int[] arr) {
+        int count = fuctorial(arr.length);
+        int max = arr.length - 1;
+        int shift = max;
+        List<String> myArr = new ArrayList<>();
+        while (count > 0) {
+            int t = arr[shift];
+            arr[shift] = arr[shift - 1];
+            arr[shift - 1] = t;
+            myArr.add(Arrays.toString(arr));
+            count--;
+            if (shift < 2) {
+                shift = max;
+            } else {
+                shift--;
+            }
+        }
+        return (ArrayList) myArr;
+    }
+
+    /**
+     * Нахождение факториала, для понимания количества комбинаций
+     */
+    public static int fuctorial(int n) {
+        return (n > 0) ? n * fuctorial(n - 1) : 1;
+    }
 }
+
