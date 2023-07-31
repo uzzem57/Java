4e23f6e7e78fe9a5031e705c9637507f71ab31ca
Denis Dunaev <uzzem57@mail.ru>
2023-07-31 22:35:44 +0300

    add Task2

--- src/SeminarExceptions/HomeWork2/Task2/Solution.java
+++ src/SeminarExceptions/HomeWork2/Task2/Solution.java
@@ -1,4 +1,14 @@
 package SeminarExceptions.HomeWork2.Task2;
 
 public class Solution {
+    public static void main(String[] args) {
+        try {
+            int d = 0;
+            int [] intArray = new int[]{1,2,3,4,5,6,77,8,9,90};
+            double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        }
+    }
 }
